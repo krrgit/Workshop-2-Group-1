02c99a83a51c48abfa0d6a3c5f69bddeb3fd3773
kevin <89359821+krrgit@users.noreply.github.com>
2022-10-19 21:27:23 -0700

    added basic arm animation

---
+++ Assets/Scripts/Animation/PlayerArmAnimator.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArmAnimator : MonoBehaviour {
+    [SerializeField] private SpriteRenderer rightArm;
+    [SerializeField] private SpriteRenderer leftArm;
+    public Transform weapon;
+
+    private bool usingRight;
+
+    public void PointArm()
+    {
+        if (usingRight)
+        {
+            rightArm.transform.parent.up = rightArm.transform.parent.position - weapon.position;
+        }
+        else
+        {
+            leftArm.transform.parent.up = leftArm.transform.parent.position - weapon.position;
+        }
+    }
+
+    public void SwitchArms(bool isRight)
+    {
+        usingRight = isRight;
+        ResetArm(!isRight);
+    }
+
+    void ResetArm(bool isRight)
+    {
+        if (isRight)
+        {
+            rightArm.transform.parent.up = Vector3.up;
+        }
+        else
+        {
+            leftArm.transform.parent.up = Vector3.up;
+        }
+    }
+
+    public void BehindPlayer(int playerLayer)
+    {
+        rightArm.sortingOrder = playerLayer - 1;
+        leftArm.sortingOrder = playerLayer - 1;
+    }
+
+    public void InfrontOfPlayer(int playerLayer)
+    {
+        rightArm.sortingOrder = playerLayer + 1;
+        leftArm.sortingOrder = playerLayer + 1;
+    }
+
+}
--- Assets/Scripts/Weapons/WeaponParent.cs
+++ Assets/Scripts/Weapons/WeaponParent.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class WeaponParent : MonoBehaviour {
 
     public SpriteRenderer charRenderer, wpnRenderer; // character renderer is needed in order to know which layer is on top or behind
+    public PlayerArmAnimator playerArms;
     private Vector3 pointerPos; // position of the pointer in world space
 
     private Vector2 direction; // direction from the player to the pointer
     private Vector3 scale; // Scale of the gun. Used to flip the sprite horizontally
-    
+    private Vector3 localPos;
+
     void Update()
     {
         GetPointerInput();
@@ -17,20 +19,25 @@ public class WeaponParent : MonoBehaviour {
 
         HorzFlip();
         RenderOrderSwitch();
+        playerArms.PointArm();
     }
 
     void HorzFlip()
     {
         scale = transform.localScale;
+        localPos = transform.localPosition;
         if (direction.x < 0)
         {
             scale.y = -Mathf.Abs(scale.y);
+            playerArms.SwitchArms(false);
         } else if (direction.x > 0)
         {
             scale.y = Mathf.Abs(scale.y);
+            playerArms.SwitchArms(true);
         }
 
         transform.localScale = scale;
+        transform.localPosition = localPos;
     }
     
     // This function sets the weapon above or below the player
@@ -39,9 +46,11 @@ public class WeaponParent : MonoBehaviour {
         if (direction.y < 0)
         {
             wpnRenderer.sortingOrder = charRenderer.sortingOrder + 1;
+            playerArms.InfrontOfPlayer(charRenderer.sortingOrder);
         } else if (direction.y >= 0)
         {
             wpnRenderer.sortingOrder = charRenderer.sortingOrder - 1;
+            playerArms.BehindPlayer(charRenderer.sortingOrder);
         }
     }
     
