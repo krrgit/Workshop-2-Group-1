5b5ba2218e33180f43c4a7535989648be501279c
kevin <89359821+krrgit@users.noreply.github.com>
2022-10-23 23:15:45 -0700

    setting up

---
+++ Assets/Scripts/Enemy/Cat/CatAttackController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAttackController : MonoBehaviour {
+    [SerializeField] private BulletHellSpawner spawner;
+    
+    void Start() {}
+    
+    void StartAttackPattern1()
+    {
+        
+    }
+    
+    
+}
