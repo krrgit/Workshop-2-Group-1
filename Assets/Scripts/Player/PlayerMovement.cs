a9cc9a1381b5a18b544ffc19d3b84e2ae5a60e68
morgansimon425 <msimon1192@sdsu.edu>
2022-10-25 15:16:58 -0700

    updated the animator

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
+
 
     Vector2 movement;
 
@@ -15,6 +17,11 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
     }
 
     private void FixedUpdate()
