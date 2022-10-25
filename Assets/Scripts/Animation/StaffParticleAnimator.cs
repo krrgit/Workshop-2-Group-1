cb876b5df1dc4859b776a6c9999f9e13261ba50f
kevin <89359821+krrgit@users.noreply.github.com>
2022-10-24 17:03:58 -0700

    Added Staff Attack Pattern

---
+++ Assets/Scripts/Animation/StaffParticleAnimator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaffParticleAnimator : MonoBehaviour {
+    [SerializeField] private float startRadius = 150;
+    [SerializeField] private float endRadius = 5;
+    [SerializeField] private float speed = 4;
+    [SerializeField] private ParticleSystem system;
+    
+    private ParticleSystem.ShapeModule shape;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        shape = system.shape;
+
+        StartCoroutine(PlayAnim());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator PlayAnim()
+    {
+        shape.radius = startRadius;
+
+        while (shape.radius > endRadius)
+        {
+            shape.radius -= Time.deltaTime * speed;
+            yield return new WaitForEndOfFrame();
+        }
+        shape.radius = endRadius;
+        yield return new WaitForSeconds(2);
+        system.Stop();
+    }
+}
--- Assets/Scripts/Enemy/Cat/CatAttackController.cs
+++ Assets/Scripts/Enemy/Cat/CatAttackController.cs
@@ -7,8 +7,13 @@ using UnityEngine;
 public class CatAttackController : MonoBehaviour {
     [SerializeField] private Animator anim;
     [SerializeField] private CatAnimEvents animEvents;
-    [SerializeField] private BulletHellSpawner spawner;
-    
+    [SerializeField] private BulletHellSpawner stompSpawner;
+    [SerializeField] private BulletHellSpawner staffSpawner;
+    [SerializeField] private bool useStomp;
+    [SerializeField] private Transform staffPoint;
+    [SerializeField] private float staffAttkDur = 10;
+
+    [SerializeField] StaffParticleAnimator staffFX;
 
     private int attackPattern;
 
@@ -16,25 +21,70 @@ public class CatAttackController : MonoBehaviour {
     
     private void OnEnable()
     {
-        animEvents.landPawDel += AttackPattern1;
+        animEvents.landPawDel += StompAttack;
     }
 
     private void OnDisable()
     {
-        animEvents.landPawDel -= AttackPattern1;
+        animEvents.landPawDel -= StompAttack;
     }
 
     void Start()
     {
-      spawner.Initialize();   
+        stompSpawner.Initialize();   
+        staffSpawner.Initialize();
     }
 
-    void AttackPattern1(Vector2 position)
+    private void Update()
     {
-        spawner.transform.position = position;
-        spawner.EmitOnce();
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            StaffCharge();
+        }
+    }
+
+    void StompAttack(Vector2 position)
+    {
+        if (!useStomp) return;
+        stompSpawner.transform.position = position;
+        stompSpawner.EmitOnce();
         
         CameraShake.Instance.Shake(0.2f,0.05f);
+    }
+
+    void StaffCharge()
+    {
+        StartCoroutine(IStaffAttack());
+    }
+
+    IEnumerator IStaffAttack()
+    {
+        // Charge
+        staffFX.gameObject.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        staffFX.gameObject.SetActive(false);
+        
+        
+        // Attack
+        staffSpawner.ToggleEmit(true);
+        float timer = staffAttkDur;
+        CameraShake.Instance.Shake(staffAttkDur, 0.02f);
+        while (timer > 0)
+        {
+            staffSpawner.transform.position = staffPoint.position;
+            timer -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        
+        staffSpawner.ToggleEmit(false);
+    }
+
+    void StaffAttack()
+    {
+        staffSpawner.transform.position = staffPoint.position;
+        staffSpawner.EmitOnce();
+        
+        CameraShake.Instance.Shake(0.5f, 0.1f);
     }
 
 
