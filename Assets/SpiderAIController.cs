2dfdb6bf96dd11349e22ec1ccca83a473c89e9bb
JohnnyR <113726617+BURG1E@users.noreply.github.com>
2022-10-27 22:20:16 -0700

    add a delay between each pattern

--- Assets/SpiderAIController.cs
+++ Assets/SpiderAIController.cs
@@ -1,45 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class SpiderAIController : MonoBehaviour
 {
     [SerializeField] private SpiderAttackController attack;
-    
-
-    public float randomNum = 0f;
-    
-
+    private bool PatternActive = false;
+    public int randomNum = 0;
+    private float timer = 0;
     void Update()
     {
         randomNum = Random.Range(1, 5);
         Debug.Log(randomNum);
-        RandomAttacksGenerator();
+        RandomAttacksGenerator(PatternActive);
+        timer -= Time.deltaTime;
     }
 
-    void RandomAttacksGenerator()
+    void RandomAttacksGenerator(bool PatternActive)
     {
+        if (timer > 0) return;
         for (int i = 0; i < randomNum; i++)
         {
-            if (randomNum == 1.0f)
-            {    
+            if (randomNum == 1 && PatternActive == false)
+            {
+                PatternActive = true;
                 attack.DoAttackOne(3);
+                timer = 3;
+                PatternActive = false;
             }
 
-            else if (randomNum == 2.0f)
+            else if (randomNum == 2 && PatternActive == false)
             {
+                PatternActive = true;
                 attack.DoAttackTwo(3);
+                timer = 3;
+                PatternActive = false;
+
             }
 
-            else if (randomNum == 3.0f)
+            else if (randomNum == 3 && PatternActive == false)
             {
+                PatternActive = true;
                 attack.DoAttackThree(3);
+                timer = 3;
+                PatternActive = false;
             }
             
-            else if (randomNum == 4.0f)
+            
+            else if (randomNum == 4 && PatternActive == false)
             {
+                PatternActive = true;
                 attack.DoAttackFour(3);
+                timer = 3;
+                PatternActive = false;
+
             }
         }
     }
 }
+
+
+
+
+
+//I CANT TAKE IT ANYMORE EATING A BURGER WITH NO HONEY MUSTARD
